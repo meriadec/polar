b7c611188ba93d5a66fd63adcd9a8ec57b58f109
meriadec <meriadec.pillet@gmail.com>
2015-08-03 21:02:33 +0200

    chore(build): optimize build

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var config = require('./webpack.config');
 
 config.plugins = [
-  new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  new webpack.optimize.UglifyJsPlugin({ minimize: true, compress: { warnings: false } })
 ];
 
 module.exports = config;
