24f825967df274baddd61fb8e48d6fd02ee95a1a
mhnlo <lenisch@hotmail.fr>
2015-09-11 16:40:02 +0200

    add content part 3

--- scenario.js
+++ scenario.js
@@ -31,7 +31,23 @@ module.exports = {
       { src: 'ecran titre.jpg' },
       { src: 'S2C1.gif' },
       { src: 'S2C2.jpg' },
-      { src: 'S2C3.jpg' }
+      { 
+        src: 'S2C3.jpg',
+        points: [
+          { x: 300, y: 650, story: 'photo' },
+          { x: 650, y: 350, story: 'suite2' }
+        ] 
+      }
+    ]
+  },
+  suite2: {
+    cells: [
+      { src: 'S2C11.jpg' },
+      { src: 'S2C12.jpg' },
+      { src: 'S2C13.jpg' },
+      { src: 'S2C14.jpg' },
+      { src: 'S2C15.jpg' },
+      { src: 'S2C16.jpg' }
     ]
   },
   cadavreTete: {
@@ -45,5 +61,50 @@ module.exports = {
     cells: [
       { src: 'S1case6a.jpg' }
     ]
-  }
+  },
+  photo: {
+    finish: 'suite1',
+    cells: [
+      { src: 'S2C4.jpg' },
+      { src: 'S2C5.jpg',
+        points: [
+          { x: 430, y: 270, story: 'Pkirin' },
+          { x: 630, y: 240, story: 'Pinconnu' },
+          { x: 720, y: 200, story: 'Peka' },
+          { x: 850, y: 240, story: 'Phector' },
+          { x: 880, y: 400, story: 'Pcarl' },
+        ] 
+      }
+    ]
+  },
+  Pkirin: {
+    finish: 'photo',
+    cells: [
+      { src: 'S2C6.jpg' }
+    ]
+  },
+  Pinconnu: {
+    finish: 'photo',
+    cells: [
+      { src: 'S2C7.jpg' }
+    ]
+  },
+  Peka: {
+    finish: 'photo',
+    cells: [
+      { src: 'S2C8.jpg' }
+    ]
+  },
+  Phector: {
+    finish: 'photo',
+    cells: [
+      { src: 'S2C9.jpg' }
+    ]
+  },
+  Pcarl: {
+    finish: 'photo',
+    cells: [
+      { src: 'S2C10.jpg' }
+    ]
+  },
 };
